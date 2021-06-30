3743c904096211b385c969d968bfdbd8344882da
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-06-30 11:17:57 -0500

    Style pages add back fuctionality

--- TabloidMVC/Models/Comments.cs
+++ TabloidMVC/Models/Comments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace TabloidMVC.Models
         public int UserProfileId { get; set; }
         public string Subject { get; set; }
         public string Content { get; set; }
+        [DisplayName("Date")]
         public DateTime CreateDateTime { get; set; }
 
     }
